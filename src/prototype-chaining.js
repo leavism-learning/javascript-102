d375a33a7d603bf04638837f0c860e91d0627b01
Huy Dang <ghuydang@gmail.com>
2023-03-15 12:07:22 -0700

    Inheriting methods

--- src/prototype-chaining.js
+++ src/prototype-chaining.js
@@ -1,6 +1,9 @@
 const animal = {
   dna: 'ATCG',
   name: 'Should be undefined but this is a string to demonstrate inheritance',
+  bark() {
+    return 'bark';
+  },
 };
 
 const dog = {
@@ -21,3 +24,4 @@ console.log(huy.age); // 24
 Object.setPrototypeOf(huy, animal);
 console.log(huy.name); // Should be undefined but this is a string to demonstrate inheritance
 console.log(huy.age); // undefined -- because we set the new huy.prototype on line 21 with setPrototypeOf
+console.log(huy.bark());
