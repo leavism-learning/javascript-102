bb6510b32a46a51416a2aaedb7643334e921b037
Huy Dang <ghuydang@gmail.com>
2023-03-15 11:28:31 -0700

    Prototype chaining with __proto__

--- src/prototype-chaining.js
+++ src/prototype-chaining.js
@@ -1,17 +1,23 @@
 const animal = {
   dna: 'ATCG',
+  name: 'Should be undefined but this is a string to demonstrate inheritance',
 };
 
 const dog = {
   name: 'Spot',
 };
 
-const huy = {};
+const huy = {
+  __proto__: {
+    age: 24,
+  },
+};
 
 Object.setPrototypeOf(dog, animal);
 console.log(Object.getPrototypeOf(dog));
 console.log(Object.getPrototypeOf(animal));
 
+console.log(huy.age); // 24
 Object.setPrototypeOf(huy, animal);
-console.log(huy.name);
-console.log(huy.dna);
+console.log(huy.name); // Should be undefined but this is a string to demonstrate inheritance
+console.log(huy.age); // undefined -- because we set the new huy.prototype on line 21 with setPrototypeOf
