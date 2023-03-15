41c0d355bf2f35f1e8d6e5e0c61a1d259c3d20f1
Huy Dang <ghuydang@gmail.com>
2023-03-15 10:39:09 -0700

    Inheritance

--- src/prototype-chaining.js
+++ src/prototype-chaining.js
@@ -6,6 +6,12 @@ const dog = {
   name: 'Spot',
 };
 
+const huy = {};
+
 Object.setPrototypeOf(dog, animal);
 console.log(Object.getPrototypeOf(dog));
 console.log(Object.getPrototypeOf(animal));
+
+Object.setPrototypeOf(huy, animal);
+console.log(huy.name);
+console.log(huy.dna);
