fa6761e0cb6c1954f49d2d65c396c5623cf7c907
Huy Dang <ghuydang@gmail.com>
2023-03-16 11:12:17 -0700

    Destructuring a nested object

--- src/destructuring.js
+++ src/destructuring.js
@@ -3,6 +3,10 @@ const huy = {
   age: 32,
   city: 'Canada',
   country: 'USA',
+  education: {
+    school: 'Dogma University',
+    degree: 'Good Boy',
+  },
 };
 
 const { name, age } = huy;
@@ -15,3 +19,9 @@ const [first, second, third] = groceries;
 const [, , cut] = groceries; // might as well do const cut = groceries[2]
 console.log(first);
 console.log(cut);
+
+const {
+  education: { degree },
+} = huy;
+
+console.log(degree);
