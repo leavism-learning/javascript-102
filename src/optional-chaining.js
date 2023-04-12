622c28876e4f597e27d5a70738fbe888a5a64153
Huy Dang <ghuydang@gmail.com>
2023-04-12 08:35:38 -0700

    Optional chaining with expressions

--- src/optional-chaining.js
+++ src/optional-chaining.js
@@ -4,3 +4,4 @@ const dude = person.name;
 console.log(foo); // Uncaught TypeError: Cannot read property 'bar' of undefined
 
 const huy = person?.name; // undefined
+const huh = person?.['na' + 'me'];
