e58cfe1e7f4d6365ad0e26b7f1ebc6408f46696c
Huy Dang <ghuydang@gmail.com>
2023-04-14 08:14:50 -0700

    Difference between nullish coalescing and default values

--- src/nullish-coalescing.js
+++ src/nullish-coalescing.js
@@ -3,3 +3,11 @@ console.log(foo); // 'bar'
 
 const bar = 0 ?? 'bar';
 console.log(foo); // 0
+
+// different from assigning a default value
+let count = 0;
+const quantity = count || 42;
+console.log(quantity); // 42 not 0
+
+const qty = count ?? 42;
+console.log(qty); // 0 not 42
