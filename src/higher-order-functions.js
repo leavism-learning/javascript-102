8f54646e57264a7dbad0d04040c090b03a993709
Huy Dang <ghuydang@gmail.com>
2023-04-19 08:21:36 -0700

    Function declarations vs expression vs hof

--- src/higher-order-functions.js
+++ src/higher-order-functions.js
@@ -1,3 +1,27 @@
+// functions
+// remember that in js functions are just objects
+// so you can pass functions into other functions
+// and be stored as variables
+
+// function declaration
+// such functions are hoisted
+function sayHi(message) {
+  return 'Said...' + message;
+}
+
+// function expression
+// such functions aren't hoisted
+const sayHello = function (message) {
+  return 'Says...' + message;
+};
+
+// So which should you use?
+// generally no difference
+// but think of it this way
+// function declarations are global scoped
+// function expressions are local scope
+
+// higher order functions
 function add(x, y) {
   return x + y;
 }
